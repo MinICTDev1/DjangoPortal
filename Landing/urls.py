67d16b676e61617bddfb58c6a72ae61641d77316
Jakana <k.jak86@yahoo.com>
2017-10-02 10:02:58 +0300

    Added navbar with login for users to be sent to the portal

--- Landing/urls.py
+++ Landing/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # ex /landing/base
+    url(r'^base/$', views.base, name='base'),
 ]
--- Landing/views.py
+++ Landing/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def base(request):
+    return render(request, 'Landing/base.html')
