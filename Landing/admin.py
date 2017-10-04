5f616b8eb71fe67e407b710f9831cac8a33541d5
Jakana <k.jak86@yahoo.com>
2017-10-04 12:16:12 +0300

    Created checkpoint for form testing

--- Landing/admin.py
+++ Landing/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Submision
+
+@admin.register(Submision)
+class LandingAdmin(admin.ModelAdmin):
+    pass
